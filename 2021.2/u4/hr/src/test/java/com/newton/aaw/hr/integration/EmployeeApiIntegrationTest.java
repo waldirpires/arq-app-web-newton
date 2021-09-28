0336987ae8f317f41ea38c5842f6c53eeb32f59e
Waldir R. Pires Jr <w.pires.ext@planerio.de>
2021-09-27 22:29:15 -0300

    Fix unused import

--- 2021.2/u4/hr/src/test/java/com/newton/aaw/hr/integration/EmployeeApiIntegrationTest.java
+++ 2021.2/u4/hr/src/test/java/com/newton/aaw/hr/integration/EmployeeApiIntegrationTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
