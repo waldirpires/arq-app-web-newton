5609b6b11d68b4f5237e52c06dfb98aea13108ba
Waldir R. Pires Jr <w.pires.ext@planerio.de>
2021-11-22 19:42:29 -0300

    22/11: U7 - Logging start

---
+++ 2021.2/u7-logging/hr/src/main/java/com/newton/aaw/hr/controller/EmployeeController.java
@@ -0,0 +1,80 @@
+package com.newton.aaw.hr.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.newton.aaw.hr.api.EmployeeDto;
+import com.newton.aaw.hr.api.EmployeesResource;
+import com.newton.aaw.hr.domain.entity.Employee;
+import com.newton.aaw.hr.service.EmployeeService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor 
+public class EmployeeController implements EmployeesResource {
+
+	private final EmployeeService employeeService;
+	
+	@Override
+	public EmployeeDto getById(@PathVariable String id) {
+		var employee = employeeService.get(id);
+		
+		return new EmployeeDto(employee);
+	}
+	
+	@Override
+	public List<EmployeeDto> getAll() {
+		var employees = employeeService.getAll();
+		
+		var employeeDtos = new ArrayList<EmployeeDto>();
+		
+		for (var employee: employees) {
+			employeeDtos.add(new EmployeeDto(employee));
+		}
+		
+		return employeeDtos;
+	}
+	
+	@Override
+	public ResponseEntity<EmployeeDto> create(
+			@RequestBody EmployeeDto employeeDto
+	) {
+		var employee = new Employee(employeeDto);
+		
+		employee = employeeService.create(employee);
+		
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(new EmployeeDto(employee));
+	}
+
+	@Override
+	public EmployeeDto update(
+			@PathVariable String id, 
+			@RequestBody EmployeeDto employeeDto
+	) {
+		var employee = new Employee(employeeDto);
+		
+		employee = employeeService.update(id, employee);
+		
+		return new EmployeeDto(employee);
+	}
+	
+	@Override
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		employeeService.delete(id);
+		
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+	}	
+}
---
+++ 2021.2/u7-logging/hr/src/main/java/com/newton/aaw/hr/service/EmployeeService.java
@@ -0,0 +1,72 @@
+package com.newton.aaw.hr.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.newton.aaw.hr.domain.entity.Employee;
+import com.newton.aaw.hr.domain.repository.EmployeeRepository;
+import com.newton.aaw.hr.exception.NotFoundException;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class EmployeeService {
+
+	private final EmployeeRepository employeeRepository;
+	
+	
+	public Employee create(Employee e) {
+		var now  = LocalDateTime.now();
+		
+		e.setCreatedAt(now);
+		e.setModifiedAt(now);
+		
+		employeeRepository.save(e);
+		
+		return e;
+	}
+	
+	public Employee update(String id, Employee u) {
+		var existing = get(id);
+
+		existing.setFirstName(u.getFirstName());
+		existing.setLastName(u.getLastName());
+		existing.setDateOfBirth(u.getDateOfBirth());
+		existing.setGender(u.getGender());
+		existing.setStartDate(u.getStartDate());
+		existing.setEndDate(u.getEndDate());
+		existing.setPosition(u.getPosition());
+		existing.setMonthlySalary(u.getMonthlySalary());
+		existing.setHourSalary(u.getHourSalary());
+		existing.setArea(u.getArea());
+		
+		existing.setModifiedAt(LocalDateTime.now());
+		
+		employeeRepository.save(existing);
+		
+		return existing; 
+	}
+	
+	public Employee get(String id) {
+		var employee = employeeRepository.findById(id);
+		
+		if (employee.isEmpty()) {
+			throw new NotFoundException("Employee with ID " + id + " not found");
+		}
+		
+		return employee.get();
+	}
+	
+	public List<Employee> getAll() {
+		return employeeRepository.findAll();
+	}
+	
+	public void delete(String id) {
+		get(id);
+		
+		employeeRepository.deleteById(id);
+	}
+}
